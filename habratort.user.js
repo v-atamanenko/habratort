24417e506e595575d559598ed492415e6d812cff
Rhaegar-Targaryen <v_ataman@ymail.com>
2014-08-31 15:55:22 +0300

    All styles minified and merged to mega.css; styles removing fix; share buttons fix; profile&tracker style fix

--- habratort.user.js
+++ habratort.user.js
@@ -48,13 +48,6 @@ if (isUserLoggedIn == true) {
 	}	
 }
 
-
-//TODO: Хабранавигатор
-//var d = document.getElementsByClassName('daily_best_posts')[0].outerHTML;
-//var c = '<div class="block fast_navigator"><div class="title">Хабранавигатор</div><form action="http://habrahabr.ru/" method="post" id="fast_navagator"><select name="category" class="category_select"><option value="null" selected="">Категория</option><option value="administration">Администрирование</option><option value="databases">Базы данных</option><option value="security">Безопасность</option><option value="browsers">Браузеры</option><option value="web-services">Веб-сервисы</option><option value="layout">Верстка</option><option value="design-and-ui">Дизайн и юзабилити </option><option value="hardware-and-gadgets">Железо и гаджеты </option><option value="advertisment">Маркетинг и реклама</option><option value="media">Медиа</option><option value="management">Менеджмент</option><option value="education">Образование</option><option value="operation-systems">Операционные системы </option><option value="programming">Программирование</option><option value="startups-and-companies">Стартапы и компании</option><option value="telecommunications">Телекоммуникации </option><option value="cms">Фреймворки и CMS</option><option value="program-langs">Языки программирования</option><option value="dessert">Десерт</option><option value="different">Разное </option></select><p></p><select name="blog" disabled="" class="blog_select"><option value="null">Хаб</option></select><div class="buttons"><input type="submit" disabled="disabled" value="Посмотреть"> </div></form></div>'; 
-//document.getElementsByClassName('daily_best_posts')[0].outerHTML = c + d;
-
-
 //Перестраиваем шапку на основе данных из левого меню
 var feedActive = "";
 var postsActive = "";
@@ -66,7 +59,7 @@ var postAddButton = "";
 
 if (window.location.href.indexOf("feed") >= 0) {
 	feedActive = "active";
-	if (isUserLoggedIn == true) { console.log('yep'); postAddButton = '<div class="add_post"><a href="/topic/add"></a></div>'; }
+	if (isUserLoggedIn == true) { postAddButton = '<div class="add_post"><a href="/topic/add"></a></div>'; }
 	if (isReadOnly == true) { postAddButton = '<div class="add_post"><a href="/sandbox/add"></a></div>'; }
 } else if (window.location.href.indexOf("posts") >= 0) {
 	postsActive = "active";
@@ -86,7 +79,7 @@ if (isUserLoggedIn == false) {
 	var oldSchoolHeader = '<div id="header"><div class="userpanel silver"><a href="http://habrahabr.ru/login/" class="login">войти</a> <a href="http://habrahabr.ru/register/">зарегистрироваться</a></div><a class="logo" href="http://habrahabr.ru/" title="На главную страницу"></a><div class="search"><form id="search_form" name="search" method="get" action="http://habrahabr.ru/search/"><input type="submit" value=""><input type="text" name="q" x-webkit-speech="" speech=""></form></div>' + oldSchoolMainMenu + '</div>';
 } else if (isUserLoggedIn == true && isReadOnly == false) {
 	var oldSchoolHeader = '<div id="header"><div class="userpanel silver"><a href="' + userLink + '" class="username">' + userName + '</a> <a href="' + settingsLink + '" class="nav-settings">настройки</a> <a href="' + logoutLink + '">выход</a><br><a href="'+ trackerLink +'" class="trackerLink">трекер</a><a href="" class="count" style="margin-right: 5px; margin-left: -7px;">' + trackerNotify + '</a> <a href="'+ dialogsLink +'" class="dialogsLink">диалоги</a><a href="" class="count" style="margin-right: 5px; margin-left: -7px;">' + dialogNotify + '</a> <a href="'+ favLink +'" class="favLink">избранное</a><br>'+ votes + '</div><a class="logo" href="http://habrahabr.ru/" title="На главную страницу"></a><div class="search"><form id="search_form" name="search" method="get" action="http://habrahabr.ru/search/"><input type="submit" value=""><input type="text" name="q" x-webkit-speech="" speech=""></form></div>' + oldSchoolMainMenu + '</div>';
-} else if (isReadOnly == true) {
+} else if (isUserLoggedIn == true && isReadOnly == true) {
 	var oldSchoolHeader = '<div id="header"><div class="userpanel silver"><a href="' + userLink + '" class="username">' + userName + '</a><sup><a href="/sandbox/add" class="charge">read-only</a></sup> <a href="' + settingsLink + '" class="nav-settings">настройки</a> <a href="' + logoutLink + '">выход</a><br><a href="'+ trackerLink +'" class="trackerLink">трекер</a><a href="" class="count" style="margin-right: 5px; margin-left: -7px;">' + trackerNotify + '</a> <a href="'+ favLink +'" class="favLink">избранное</a></div><a class="logo" href="http://habrahabr.ru/" title="На главную страницу"></a><div class="search"><form id="search_form" name="search" method="get" action="http://habrahabr.ru/search/"><input type="submit" value=""><input type="text" name="q" x-webkit-speech="" speech=""></form></div>' + oldSchoolMainMenu + '</div>';
 }
 
@@ -98,40 +91,17 @@ document.getElementsByClassName('content_left')[0].innerHTML = postAddButton + c
 
 //Удаляем новый стиль
 var styles = document.getElementsByTagName('link');
-    for (index = 0; index < styles.length; ++index) {
-    styles[index].parentNode.removeChild(styles[index]);
-}
-
-var styles = document.getElementsByTagName('link');
-    for (index = 0; index < styles.length; ++index) {
-    styles[index].parentNode.removeChild(styles[index]);
-}
-
-var styles = document.getElementsByTagName('link');
-    for (index = 0; index < styles.length; ++index) {
-    styles[index].parentNode.removeChild(styles[index]);
-}
-
-var styles = document.getElementsByTagName('link');
-    for (index = 0; index < styles.length; ++index) {
-    styles[index].parentNode.removeChild(styles[index]);
+while(styles.length > 0) {
+    styles[0].parentNode.removeChild(styles[0]);
 }
 
 //Добавляем измененный старый стиль, удаляем меню слева, логотипы под футером и название страницы
-var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/all.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/forms.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/highlight.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/hubs-all.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/posts.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/companies.css">';
-headHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/post_common.css">';
-document.getElementsByTagName('head')[0].innerHTML = headHTML;
+document.getElementsByTagName('head')[0].innerHTML += '<link type="text/css" rel="stylesheet" href="http://blackbloodm.org/oldhabr/data/mega.css">';
 
-document.getElementById('layout').removeChild(document.getElementById('navbar'));
-document.getElementById('layout').removeChild(document.getElementById('navbar_overlay'));
+document.getElementById('navbar').style.display = 'none';
+document.getElementById('navbar_overlay').style.display = 'none';
 
-if (document.getElementsByClassName('page_head')[0]) {document.getElementsByClassName('page_head')[0].parentNode.removeChild(document.getElementsByClassName('page_head')[0]); }
-document.getElementsByClassName('footer_logos')[0].parentNode.removeChild(document.getElementsByClassName('footer_logos')[0]);
+if (document.getElementsByClassName('page_head')[0]) {document.getElementsByClassName('page_head')[0].style.display = 'none'; }
+document.getElementsByClassName('footer_logos')[0].style.display = 'none';
 
-if (document.getElementsByClassName('bottom_promo_blocks')[0]) {document.getElementsByClassName('bottom_promo_blocks')[0].parentNode.removeChild(document.getElementsByClassName('bottom_promo_blocks')[0]);}
+if (document.getElementsByClassName('bottom_promo_blocks')[0]) {document.getElementsByClassName('bottom_promo_blocks')[0].style.display = 'none';}
